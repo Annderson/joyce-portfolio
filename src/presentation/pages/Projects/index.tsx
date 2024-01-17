eed32f7c06a59b0be22177aab36cd85c4dfb1c41
Anderson Rodrigues Lima <anderson.lima@NMAC-3376.home>
2024-01-17 08:36:24 -0300

    feat: create projects page

---
+++ src/presentation/pages/Projects/index.tsx
@@ -0,0 +1,13 @@
+import Menu from '@/presentation/components/Menu'
+
+import * as S from './styles'
+
+const Projects = () => (
+  <Menu>
+    <S.Container>
+      <h3>Projects</h3>
+    </S.Container>
+  </Menu>
+)
+
+export default Projects
