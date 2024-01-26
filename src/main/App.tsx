372f43cec04e7dec7df65d019fc908b843c86c0e
Anderson Rodrigues Lima <anderson.lima@NMAC-3376.local>
2024-01-26 18:43:04 -0300

    refactor: change images imports

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyles, theme } from '@/styles'
+
+import '@/assets/imports/constructionTechniques'
+import '@/assets/imports/constructionTechniquesB'
+import '@/assets/imports/projectPark'
+import '@/assets/imports/projectSchool'
+
+
 import Router from './routes'
 
 const App: React.FC = () => {
